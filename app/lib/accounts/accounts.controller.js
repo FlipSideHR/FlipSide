c5e0a0bdfe861b24bbd7f97bc7e7532d80bc7398
Michael Symmes <msymmes@gmail.com>
2015-07-10 16:43:41 -0400

    Wired in more stuff - wallet data/order data not working in this implementation - orders are.

--- app/lib/accounts/accounts.controller.js
+++ app/lib/accounts/accounts.controller.js
@@ -1,15 +1,28 @@
 angular.module('flipside.accounts', [])
-  .controller('AccountsController', ['$scope', 'AccountsService', function($scope, AccountsService){
+  .controller('AccountsController', ['$scope', '$interval', 'AccountsService', function($scope, $interval, AccountsService){
 
     // hold order data
     $scope.order = {};
+    
+    $scope.wallets = [];    
+    $scope.ordersList = [];
+
+    AccountsService.fetchWallets(function(wallets){
+      $scope.wallets = wallets;                            
+    });
+
+    AccountsService.fetchOrders(function(orders){
+      console.log(orders.list);
+      $scope.ordersList = orders.list;                            
+      console.log($scope.ordersList);
+    });
 
     $scope.buy = function(){
       AccountsService.buy($scope.order.price, $scope.order.size);
     };
     
     $scope.sell = function(){
-      AccountsService.buy($scope.order.price, $scope.order.size);
+      AccountsService.sell($scope.order.price, $scope.order.size);
     };
 
   }]);
---
+++ app/lib/services/sandbox.js
@@ -0,0 +1,6 @@
+module.exports = {
+  passphrase: 'ninjasoup',
+  key: 'cf8ee80e86bae0689f871589598085a5',
+  b64secret: 'AWXiScV7sZcubjcMZE6/EgXaXToNOM4CfRqpyHkQCMYxsGICkRUewR21+2qwaSmCbQpqgtYNachwtKxdKgYM5Q==',
+  uri: 'https://api-public.sandbox.exchange.coinbase.com'
+};
