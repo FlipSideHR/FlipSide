a5d46ae3944c096dec7e95f4f8891e12a5517ecd
Michael Symmes <msymmes@gmail.com>
2015-07-10 17:00:58 -0400

    Updated credential files

--- app/lib/services/sandbox.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  passphrase: 'ninjasoup',
-  key: 'cf8ee80e86bae0689f871589598085a5',
-  b64secret: 'AWXiScV7sZcubjcMZE6/EgXaXToNOM4CfRqpyHkQCMYxsGICkRUewR21+2qwaSmCbQpqgtYNachwtKxdKgYM5Q==',
-  uri: 'https://api-public.sandbox.exchange.coinbase.com'
-};
